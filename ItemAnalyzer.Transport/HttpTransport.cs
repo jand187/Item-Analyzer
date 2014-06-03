3113032091098e65ce81648a6fa6f9a4a6fe0067
Jan Daniel Andersen <jand187@gmail.com>
2014-06-03 13:01:33 +0200

    Added HttpTransport dummy.

---
+++ ItemAnalyzer.Transport/HttpTransport.cs
@@ -0,0 +1,34 @@
+﻿namespace ItemAnalyzer.Transport
+{
+	public interface IHttpTransport
+	{
+		InventoryPage GetInventory();
+	}
+
+	public class HttpTransport : IHttpTransport
+	{
+		public InventoryPage GetInventory()
+		{
+			return new InventoryPage
+			{
+				Id = 1,
+				Name = "first page"
+			};
+		}
+	}
+
+	public class InventoryPage
+	{
+		public int Id
+		{
+			get;
+			set;
+		}
+
+		public string Name
+		{
+			get;
+			set;
+		}
+	}
+}
