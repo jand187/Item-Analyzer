e89721d3399c020eb7fc6d53687cd01652b77fc4
Jan Daniel Andersen <jand187@gmail.com>
2014-06-03 13:03:18 +0200

    Added HaveBoxControllerActivator.

---
+++ ItemAnalyzer.Website/HaveBox/HaveBoxControllerActivator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Dispatcher;
+using HaveBox;
+
+namespace ItemAnalyzer.Website.HaveBox
+{
+	public class HaveBoxControllerActivator : IHttpControllerActivator
+	{
+		private readonly Container _container;
+
+		public HaveBoxControllerActivator(Container container)
+		{
+			_container = container;
+		}
+
+		public IHttpController Create(HttpRequestMessage request, HttpControllerDescriptor controllerDescriptor, Type controllerType)
+		{
+			return _container.GetInstance(controllerType) as IHttpController;
+		}
+	}
+}
