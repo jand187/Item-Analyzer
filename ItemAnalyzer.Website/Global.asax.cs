4dede03843d144aa1c34c6beb9868433b60c1bb3
Jan Daniel Andersen <jand187@gmail.com>
2014-06-03 13:04:35 +0200

    Configured HaveBox.

--- ItemAnalyzer.Website/Global.asax.cs
+++ ItemAnalyzer.Website/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
-using HaveBox;
-using HaveBox.WebExtensions;
-using ItemAnalyzer.Website.Api;
 
 namespace ItemAnalyzer.Website
 {
@@ -12,16 +8,7 @@ namespace ItemAnalyzer.Website
 		protected void Application_Start()
 		{
 			GlobalConfiguration.Configure(WebApiConfig.Register);
-
-			GlobalConfiguration.Configuration.DependencyResolver = new HaveBoxDependencyResolver(CreateContainerAndBootstrap());
-		}
-
-		private IContainer CreateContainerAndBootstrap()
-		{
-			var container = new Container();
-			container.Configure(x => { x.For<UpdateController>().Use<UpdateController>(); });
-
-			return container;
+			GlobalConfiguration.Configure(HaveBoxConfig.Register);
 		}
 	}
 }
