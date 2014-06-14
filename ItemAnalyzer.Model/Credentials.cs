6ca6d8cb797b085b1435c530a94e6c8c17eb4876
Jan Daniel Andersen <jand187@gmail.com>
2014-06-14 09:55:59 +0200

    Added Credentials.

---
+++ ItemAnalyzer.Model/Credentials.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Security;
+
+namespace ItemAnalyzer.Model
+{
+	public class Credentials
+	{
+		private string password;
+
+		public string Username { get; set; }
+
+		public string Password
+		{
+			set
+			{
+				SecurePassword = new SecureString();
+				value.ToCharArray().ToList().ForEach(SecurePassword.AppendChar);
+			}
+		}
+
+		public SecureString SecurePassword { get; set; }
+	}
+}
