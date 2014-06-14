2cc2162b6d7c07b0e121d5ef1335f0224e001511
Jan Daniel Andersen <jand187@gmail.com>
2014-06-14 09:58:50 +0200

    Added SecureStringExtensions.

---
+++ ItemAnalyzer.Transport/SecureStringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace ItemAnalyzer.Transport
+{
+	public static class SecureStringExtensions
+	{
+		public static string UnWrap(this SecureString secret)
+		{
+			if (secret == null)
+				throw new ArgumentNullException("secret");
+
+			var ptr = Marshal.SecureStringToCoTaskMemUnicode(secret);
+			try
+			{
+				return Marshal.PtrToStringUni(ptr);
+			}
+			finally
+			{
+				Marshal.ZeroFreeCoTaskMemUnicode(ptr);
+			}
+		}
+	}
+}
