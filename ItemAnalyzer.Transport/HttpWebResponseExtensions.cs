eb459cff33564930a43698a3791d7567abc73d54
Jan Daniel Andersen <jand187@gmail.com>
2014-06-14 10:24:59 +0200

    Added HttpWebResponseExtensions.

---
+++ ItemAnalyzer.Transport/HttpWebResponseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace ItemAnalyzer.Transport
+{
+	public static class HttpWebResponseExtensions
+	{
+		public static string ReadAll(this HttpWebResponse response)
+		{
+			using (var reader = new StreamReader(response.GetResponseStream()))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
