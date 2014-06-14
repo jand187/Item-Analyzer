674bb658549ad8088f75c4db1af63ea11e7b20ee
Jan Daniel Andersen <jand187@gmail.com>
2014-06-14 09:58:36 +0200

    Added HttpMethod.

---
+++ ItemAnalyzer.Transport/HttpMethod.cs
@@ -0,0 +1,17 @@
+using System.Linq;
+
+namespace ItemAnalyzer.Transport
+{
+	public class HttpMethod
+	{
+		public static string Get
+		{
+			get { return "GET"; }
+		}
+
+		public static string Post
+		{
+			get { return "POST"; }
+		}
+	}
+}
